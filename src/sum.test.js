24489397eeb60100ccf7d8256670faf03a03a3d2
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-05-23 20:51:13 +0530

    created test file to test sum function

---
+++ src/sum.test.js
@@ -0,0 +1,12 @@
+import sum from "./sum";
+
+test("testing for sum function", () => {
+    let a = 10, b = 10, output = 20
+    expect(sum(a, b)).toBe(output)
+})
+
+
+test("testing for sum function 2", () => {
+    let a = 100, b = 200, output = 300
+    expect(sum(a, b)).toBe(output)
+})
