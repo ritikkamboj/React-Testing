6053f0d27869d4d2951ea91dca7de043fd6c4e2f
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-05-24 18:36:11 +0530

    snapshot testing , used only when code is almost completed and going to deploy soon

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   const [data, setData] = useState("");
   return (
     <div className="App">
-      <h1>Testing of click event on button</h1>
+      <h1>jest testing</h1>
+
       <button onClick={() => setData("updated data")}>Update data</button>
       <h1>{data}</h1>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,51 +1,7 @@
-import { fireEvent, render, screen } from "@testing-library/react"
-import App from "./App"
-import dbClean from "./dbClean"
+import { render } from "@testing-library/react";
+import App from "./App";
 
-// beforeAll(() => {
-//   console.log("**** beforeall hook****")
-// })
-
-beforeEach(() => {
-  // console.log("****beforeEach hook*****")
-  dbClean();
+test("snapshot for app componenet file ", () => {
+  let box = render(<App />);
+  expect(box).toMatchSnapshot()
 })
-
-test("click event test case 1", () => {
-  console.log("this is 1 ")
-  render(<App />)
-  const btn = screen.getByRole("button");
-  fireEvent.click(btn);
-  expect(screen.getByText("updated data")).toBeInTheDocument();
-
-})
-
-
-test("click event test case 2", () => {
-  console.log("this is 2 ")
-
-  render(<App />)
-  const btn = screen.getByRole("button");
-  fireEvent.click(btn);
-  expect(screen.getByText("updated data")).toBeInTheDocument();
-
-})
-
-test("click event test case 3", () => {
-  console.log("this is 3 ")
-
-  render(<App />)
-  const btn = screen.getByRole("button");
-  fireEvent.click(btn);
-  expect(screen.getByText("updated data")).toBeInTheDocument();
-
-})
-
-// afterAll(() => {
-//   console.log("**** afterAll hook")
-// })
-
-
-// afterEach(() => {
-//   console.log("**** afterEach hook")
-// })
