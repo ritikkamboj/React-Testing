0356cf59d1cddb3ed5d753b615c42909665a3ca6
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-05-25 22:46:31 +0530

    getAllByLabelText

--- src/App.js
+++ src/App.js
@@ -12,13 +12,27 @@ function App() {
 
   return (
     <div className="App">
-      <h1>getByLableText</h1>
-      <label htmlFor="user-name">username</label>
-      <input type="text" id='user-name' defaultValue="aashu" />
+      <h1>getAllByLabelText</h1>
+      <label htmlFor="username">username</label>
+      <input type="text" id='username' defaultValue="anil" />
+
+      <label htmlFor="username2">username</label>
+      <input type="text" id='username2' defaultValue="anil" />
+
+      <label htmlFor="username3">username</label>
+      <input type="text" id='username3' defaultValue="anil" />
 
 
-      <label htmlFor="skills">skills</label>
-      <input type="checkbox" id='skills' />
+      <label htmlFor="skill">skill</label>
+      <input type="checkbox" id='skill' defaultChecked={true} />
+
+      <label htmlFor="skill2">skill</label>
+      <input type="checkbox" id='skill2' defaultChecked={true} />
+
+
+      <label htmlFor="skill3">skill</label>
+      <input type="checkbox" id='skill3' defaultChecked={true} />
+
 
 
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,26 @@
 const { render, screen } = require("@testing-library/react")
 import App from "./App"
 
-test("getByLabelText", () => {
+
+test("input test case ", () => {
   render(<App />)
-  const input = screen.getByLabelText("username")
-  expect(input).toBeInTheDocument();
-  expect(input).toHaveValue("aashu")
+  const inputs = screen.getAllByLabelText("username");
+
+
+  for (let i = 0; i < inputs.length; i++) {
+    expect(inputs[i]).toBeInTheDocument();
+    expect(inputs[i]).toHaveValue("anil");
+  }
 })
 
 
-
-test("getByLabelText 2", () => {
+test("input test case for check box ", () => {
   render(<App />)
-  const checkbox = screen.getByLabelText("skills")
-  expect(checkbox).toBeInTheDocument()
+  const checkboxes = screen.getAllByLabelText("skill");
 
+
+  for (let i = 0; i < checkboxes.length; i++) {
+    expect(checkboxes[i]).toBeInTheDocument();
+    expect(checkboxes[i]).toBeChecked();
+  }
 })
