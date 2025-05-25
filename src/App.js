e60beabfe69e67130357e8da445b71103a420085
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-05-25 19:29:59 +0530

    using of semantic elements multiple times and non semantic element with role

--- src/App.js
+++ src/App.js
@@ -12,9 +12,16 @@ function App() {
 
   return (
     <div className="App">
-      <h1>getByRole</h1>
-      <input type="text" defaultValue="hello" disabled />
-      <button>Click me </button>
+      <h1>Mutiple items with Role</h1>
+      <h1>Custom Role </h1>
+      <button>Click 1</button>
+      <button>Click 2</button>
+      <label htmlFor="input1">User Name</label>
+      <input type="text" id='input1' />
+      <label htmlFor="input2">User Age</label>
+      <input type="text" id='input2' />
+      <div role='dummy'>Dummy Text </div>
+
 
 
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,16 @@
-const { render, screen } = require("@testing-library/react")
+const { render, screen } = require("@testing-library/react");
 import App from "./App"
 
-test("get by role", () => {
+test("getByRole Testing", () => {
   render(<App />)
-  const inputField = screen.getByRole("textbox");
-  const btn = screen.getByRole("button")
-  expect(inputField).toBeInTheDocument();
-  expect(inputField).toHaveValue("hello")
-  expect(inputField).toBeDisabled();
-
-  expect(btn).toBeInTheDocument();
-
+  const btn1 = screen.getByRole("button", { name: "Click 1" });
+  const btn2 = screen.getByRole("button", { name: "Click 2" });
+  const input1 = screen.getByRole("textbox", { name: "User Name" });
+  const input2 = screen.getByRole("textbox", { name: "User Age" });
+  const div1 = screen.getByRole("dummy");
+  expect(div1).toBeInTheDocument()
+  expect(btn1).toBeInTheDocument();
+  expect(btn2).toBeInTheDocument();
+  expect(input1).toBeInTheDocument();
+  expect(input2).toBeInTheDocument();
 })
-
