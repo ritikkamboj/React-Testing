629af71563542312f51d79ae6768c120fe561ea5
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-05-26 16:15:42 +0530

    getByAltText and getAllByAltText for images

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
 
   return (
     <div className="App">
-      <h1>getByTitle and getAllByTitle</h1>
-      <button title='click'>Click </button>
-      <span title='spade'>&#9824;</span>
-      <span title='spade'>&#9824;</span>
+      <h1>getByAltText and getAllByAltText</h1>
+      <img src="https://cdn.pixabay.com/photo/2015/09/16/08/55/online-942406_1280.jpg" alt="dummy image" />
+      <img src="https://cdn.pixabay.com/photo/2015/09/16/08/55/online-942406_1280.jpg" alt="dummy image" />
+
 
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,16 @@
 const { render, screen } = require("@testing-library/react")
 import App from "./App"
 
-test("testing of button with title", () => {
-  render(<App />)
-  const btn = screen.getByTitle("click");
-  expect(btn).toBeInTheDocument();
-})
+// test("testing of image tag  with Alt", () => {
+//   render(<App />)
+//   const btn = screen.getByAltText("dummy image");
+//   expect(btn).toBeInTheDocument();
+// })
 
 
 test("testing of span tag", () => {
   render(<App />)
-  const span = screen.getAllByTitle("spade");
+  const span = screen.getAllByAltText("dummy image");
 
   for (let i = 0; i < span.length; i++) {
     expect(span[i]).toBeInTheDocument();
