878c7859f7830ca885f4413e597fe592ac4b53e0
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-05-27 17:08:18 +0530

    use of act function to avaoid testing failure due to late update , sometimes happens

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,15 @@
 const { render, screen } = require("@testing-library/react")
 import userEvent from "@testing-library/user-event"
 import App from "./App"
+import { act } from "react";
 
 test("on change event testing", async () => {
   userEvent.setup();
   render(<App />)
   const el = screen.getByRole("textbox");
-  await userEvent.type(el, "aashu");
+  await act(async () => {
+    await userEvent.type(el, "aashu");
+  })
   expect(screen.getByText("aashu")).toBeInTheDocument();
 
 })
